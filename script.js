e862ade2a30895ad327081cefb0a5586d66f077e
antonie <90637590+antonie1212@users.noreply.github.com>
2023-11-14 22:13:44 +0200

    am combinat functia mea de cautare duma email si nume curs si cu funtia lui nichita de combinare nume prenume

--- script.js
+++ script.js
@@ -44,83 +44,63 @@ const createTableRows = () => {
 createTableRows();
 
 
-// nichita
-const searchField = document.getElementById('searchField');
-const tableRows = document.getElementById('studentsTableBody').getElementsByTagName('tr');
-const searchBtn = document.getElementById('searchBtn');
 
-const searchByName = function () {
-    const searchTerm = searchField.value.toLowerCase();
+/// Anton si Nichita
 
-    for (let i = 0; i < tableRows.length; i++) {
-        const cells = tableRows[i].getElementsByTagName("td");
+document.addEventListener('DOMContentLoaded', function() {
+  const searchBtn = document.getElementById("searchBtn");
+  const searchField = document.getElementById("searchField");
+  const tableRows = document.getElementById('studentsTableBody').getElementsByTagName('tr');
 
-        const nameCell = cells[0];
-
-        if (nameCell) {
-            const nameText = nameCell.textContent.toLowerCase();
-
-            if (searchTerm === '' || nameText.includes(searchTerm)) {
-                tableRows[i].style.backgroundColor = searchTerm === '' ? 'transparent' : 'yellow';
-            } else {
-                tableRows[i].style.backgroundColor = 'transparent';
-            }
-        }
-    }
-};
-
-
-searchBtn.addEventListener("click", searchByName);
-=======
-
-
-
-
-// Antonie - Select Username By Name;
-document.addEventListener("DOMContentLoaded", function () {
-    const searchBtn = document.getElementById("searchBtn");
-    const searchField = document.getElementById("searchField");
-    const tableRows = document.getElementsByTagName("tr");
-  
-    searchBtn.addEventListener("click", function () {
+  searchBtn.addEventListener("click", function () {
       const searchTerm = searchField.value.toLowerCase();
-  
+
       for (let i = 0; i < tableRows.length; i++) {
-        tableRows[i].style.backgroundColor = "transparent"; // Resetting all rows to transparent
-  
-        const emailCell = tableRows[i].getElementsByTagName("td")[1]; // Cell containing the email
-        const courseCell = tableRows[i].getElementsByTagName("td")[2]; // Cell containing the course
-  
-        if (emailCell && courseCell) {
-          const emailText = emailCell.textContent.toLowerCase();
-          const courseText = courseCell.textContent.toLowerCase();
-  
-          if (emailText.includes(searchTerm) || courseText.includes(searchTerm)) {
-            tableRows[i].style.backgroundColor = "yellow"; // Highlighting rows that match email or course name
+          const nameCell = tableRows[i].getElementsByTagName("td")[0]; // Cell containing the name
+          const emailCell = tableRows[i].getElementsByTagName("td")[1]; // Cell containing the email
+          const courseCell = tableRows[i].getElementsByTagName("td")[2]; // Cell containing the course
+
+          if (nameCell && emailCell && courseCell) {
+              const nameText = nameCell.textContent.toLowerCase();
+              const emailText = emailCell.textContent.toLowerCase();
+              const courseText = courseCell.textContent.toLowerCase();
+
+              if (
+                  nameText.includes(searchTerm) ||
+                  emailText.includes(searchTerm) ||
+                  courseText.includes(searchTerm)
+              ) {
+                  tableRows[i].style.backgroundColor = "yellow"; // Highlighting rows that match any field
+              } else {
+                  tableRows[i].style.backgroundColor = "transparent";
+              }
           }
-        }
       }
-    });
-  
-    searchField.addEventListener("input", function () {
-      const searchTerm = searchField.value.toLowerCase();
-  
-      for (let i = 0; i < tableRows.length; i++) {
-        const emailCell = tableRows[i].getElementsByTagName("td")[1]; // Cell containing the email
-        const courseCell = tableRows[i].getElementsByTagName("td")[2]; // Cell containing the course
-  
-        if (emailCell && courseCell) {
-          const emailText = emailCell.textContent.toLowerCase();
-          const courseText = courseCell.textContent.toLowerCase();
-  
-          if (emailText.includes(searchTerm) || courseText.includes(searchTerm)) {
-            tableRows[i].style.backgroundColor = "yellow"; // Highlighting rows that match email or course name
-          } else {
-            tableRows[i].style.backgroundColor = "transparent";
-          }
-        }
-      }
-    });
   });
-  
 
+  searchField.addEventListener("input", function () {
+      const searchTerm = searchField.value.toLowerCase();
+
+      for (let i = 0; i < tableRows.length; i++) {
+          const nameCell = tableRows[i].getElementsByTagName("td")[0]; 
+          const emailCell = tableRows[i].getElementsByTagName("td")[1]; 
+          const courseCell = tableRows[i].getElementsByTagName("td")[2]; 
+
+          if (nameCell && emailCell && courseCell) {
+              const nameText = nameCell.textContent.toLowerCase();
+              const emailText = emailCell.textContent.toLowerCase();
+              const courseText = courseCell.textContent.toLowerCase();
+
+              if (
+                  nameText.includes(searchTerm) ||
+                  emailText.includes(searchTerm) ||
+                  courseText.includes(searchTerm)
+              ) {
+                  tableRows[i].style.backgroundColor = "yellow"; 
+              } else {
+                  tableRows[i].style.backgroundColor = "transparent";
+              }
+          }
+      }
+  });
+});
