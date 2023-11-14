7120a358e6461a589d2219e9f6e8f5f722c1c22b
antonie <90637590+antonie1212@users.noreply.github.com>
2023-11-14 15:59:41 +0200

    Defoulf Files

---
+++ script.js
@@ -0,0 +1,17 @@
+// Importati toti utilizatorii din json in tabel si apoi inputul search introduceti un nume, email sau curs  apasand pe buton utilizatorii
+// gasiti conform inputului se va colora cu un background galben, conformaginii din mapa img
+
+let users = [
+
+{"first_name":"Cornelius","email":"csummerley0@nydailynews.com","student_course":"Voltsillam"},
+{"first_name":"Stephi","email":"swagen1@usda.gov","student_course":"Vagram"},
+{"first_name":"Hinze","email":"hfoulser2@techcrunch.com","student_course":"Konklux"},
+{"first_name":"Brew","email":"bshory3@house.gov","student_course":"Gembucket"},
+{"first_name":"Andrew","email":"ahatliffe4@howstuffworks.com","student_course":"Treeflex"},
+{"first_name":"Ruddy","email":"rcastrillo5@ustream.tv","student_course":"Voltsillam"},
+{"first_name":"Jo ann","email":"jyoung6@joomla.org","student_course":"Alpha"},
+{"first_name":"Pavlov","email":"pdudeney7@youtube.com","student_course":"Matsoft"},
+{"first_name":"Rhianon","email":"rkeer8@moonfruit.com","student_course":"Stronghold"},
+{"first_name":"Montague","email":"mladlow9@bbb.org","student_course":"Veribet"}
+
+]
